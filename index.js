8888c74e516342ce1bd8e704793596e05c042bbb
kyk <86296198+Msolmaz4@users.noreply.github.com>
2023-07-12 23:48:22 +0200

    versuch

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log("deneme")
+
+/*
+const day = prompt("gun girin")
+const time= prompt("yaman girin")
+
+
+if(day =="pazar"){console.log("kapali")}
+if(day == "pazartesi" && 8 < time <17) { console.log("acik")}
+
+*/
+
+
+
+
